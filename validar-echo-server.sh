a3ae73dd545a277316bc09352405a69ab2f1ac1a
LijsTh <lijsth@gmail.com>
2024-08-26 17:58:53 -0300

    Added validation script
    Used docker to not expose local host ports + not installing NC taking advatange of the docker network already in place. Chose alpine as OS as its really lightweight.

---
+++ validar-echo-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NETWORK="tp0_testing_net"
+SERVER="server"
+PORT=12345
+MSG="Hello server im a test"
+
+RESPONSE=$(docker run --rm --network $NETWORK alpine sh -c "echo $MSG | nc $SERVER $PORT")
+
+if [ "$RESPONSE" == "$MSG" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
