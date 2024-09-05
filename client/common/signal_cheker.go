ac8dd7dd979b4ec1adb9ab46c8452dc53fea69a8
LijsTh <lijsth@gmail.com>
2024-09-05 12:59:16 -0300

    typo in log

--- client/common/signal_cheker.go
+++ client/common/signal_cheker.go
@@ -9,7 +9,7 @@ import (
 func wait_for_signal(ctx context.Context, connection *net.Conn, finished_iter chan bool, stopped *bool) {
 	select {
 		case <-ctx.Done():
-			log.Infof("action: SIGTERM | result: sucess")
+			log.Infof("action: SIGTERM | result: success")
 			err := (*connection).Close()
 			if err == nil {
 				log.Infof("action: close_connection | result: success")
