8dc7fd1dcbfc78b5d198c6110537d2afd78a0394
LijsTh <lijsth@gmail.com>
2024-09-05 01:12:45 -0300

    client close (forgot)

--- client/common/client.go
+++ client/common/client.go
@@ -100,6 +100,7 @@ loop:
 			log.Infof("action: SIGTERM Received | result: success | client_id: %v", c.config.ID)
 			break loop 
 		default:
+			c.conn.Close()
 			time.Sleep(c.config.LoopPeriod)
 		}
 	}
