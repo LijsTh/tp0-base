16fd64e1629123180301aff9c1d242beb5acd811
LijsTh <lijsth@gmail.com>
2024-09-05 12:58:35 -0300

    logs agains

--- client/common/signal_cheker.go
+++ client/common/signal_cheker.go
@@ -9,7 +9,7 @@ import (
 func wait_for_signal(ctx context.Context, connection *net.Conn, finished_iter chan bool, stopped *bool) {
 	select {
 		case <-ctx.Done():
-			log.Infof("action: SIGTERM | result: received")
+			log.Infof("action: SIGTERM | result: sucess")
 			err := (*connection).Close()
 			if err == nil {
 				log.Infof("action: close_connection | result: success")
